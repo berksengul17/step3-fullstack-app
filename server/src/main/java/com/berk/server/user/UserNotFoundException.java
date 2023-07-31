abd4e2543ba4c6de4dd0750daa155e7c29db7507
berk <berksengul17@gmail.com>
2023-08-01 00:33:32 +0300

    create custom exception

--- server/src/main/java/com/berk/server/user/UserNotFoundException.java
+++ server/src/main/java/com/berk/server/user/UserNotFoundException.java
@@ -1,14 +1,7 @@
 package com.berk.server.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class UserNotFoundException extends RuntimeException{
-    private String message;
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
