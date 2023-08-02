71066839afe8978e2d1adf3b873241259120d8f9
berk <berksengul17@gmail.com>
2023-08-02 14:58:13 +0300

    add new custom query

--- server/src/main/java/com/berk/server/user/UserRepository.java
+++ server/src/main/java/com/berk/server/user/UserRepository.java
@@ -1,8 +1,16 @@
 package com.berk.server.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN " +
+            "true ELSE false END " +
+            "FROM User u " +
+            "WHERE u.name = ?1")
+    boolean existsByName(String name);
 }
